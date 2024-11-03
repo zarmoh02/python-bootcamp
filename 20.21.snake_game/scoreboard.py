e7b5f7a526cc625cb6fd3c127dc580a2de6e2b5d
Zahra Mohammadi <139892836+zarmoh02@users.noreply.github.com>
2024-11-03 23:52:57 +0330

    Update scoreboard.py
    I added the ability of keeping highest score using a text file.

--- 20.21.snake_game/scoreboard.py
+++ 20.21.snake_game/scoreboard.py
@@ -1,9 +1,9 @@
 from turtle import Turtle
 
 START_POSITION = (0, 270)
-START_SCORE = 0
 ALIGNMENT = "center"
-FONT = ("Courier", 20, "bold")
+FONT = ("Courier", 16, "normal")
+
 
 class Scoreboard(Turtle):
 
@@ -14,14 +14,27 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.goto(START_POSITION)
-        self.score = START_SCORE
-        self.increase_score()
+        self.score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score:{self.score}, Highest score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.score += 1
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
