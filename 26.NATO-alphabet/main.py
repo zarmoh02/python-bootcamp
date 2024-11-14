a26134f2ac518f867d122056d89c48b820d9ad7a
Zahra Mohammadi <139892836+zarmoh02@users.noreply.github.com>
2024-11-15 00:12:42 +0330

    Update main.py
    I added Exception Handling in the NATO Phonetic Alphabet Project, so if the user typed anything rather than alphabet. the code doesn't crash.

--- 26.NATO-alphabet/main.py
+++ 26.NATO-alphabet/main.py
@@ -1,11 +1,17 @@
 import pandas
 
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-# TODO 1. Create a dictionary in this format:
 nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter your word:").upper()
-coded_word = [nato_dict[letter] for letter in word]
-print(coded_word)
+def nato_generator():
+    word = input("Enter your word:").upper()
+    try:
+        coded_word = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+    else:
+        print(coded_word)
+    finally:
+        nato_generator()
+
+nato_generator()
